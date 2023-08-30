203a4bb874e31bbea7babf6bea79a0569960fce1
Rahul Haral <rharal@flextrade.com>
2023-08-30 11:27:55 +0530

    rat in maze

---
+++ ratInMaze.js
@@ -0,0 +1,87 @@
+let count = 0;
+function findPath(matrix, r, i, visited = {}) {
+	//recieved matrix
+	// need to check adjuscent elements has 1 value or not
+	// let's find left/right/down or up elements has 1
+	// if found do same thing for the remaining one recusrsive call to an same method with updated data
+	const numOfRows = matrix.length;
+	const n = matrix[0].length;
+	count++;
+	if (r == numOfRows - 1 && i == n - 1) {
+		return true; // this means rat is able to reach to the hole
+	}
+	visited[`${r}${i}`] = true;
+	const li = i - 1;
+	const ri = i + 1;
+	const ur = r - 1;
+	const dr = r + 1;
+	if (!visited[`${r}${ri}`] && ri < n && matrix[r][ri] == 1) {
+		if (findPath(matrix, r, ri, visited)) {
+			return true;
+		} // if right element on same row has 1
+	}
+
+	if (!visited[`${r}${li}`] && li > -1 && matrix[r][li] == 1) {
+		if (findPath(matrix, r, li, visited)) {
+			return true;
+		} // if left element on same row has 1
+	}
+
+	if (!visited[`${dr}${i}`] && dr < numOfRows && matrix[dr][i] == 1) {
+		if (findPath(matrix, dr, i, visited)) {
+			return true;
+		} // if down element on below row has 1
+	}
+
+	if (!visited[`${ur}${i}`] && ur > -1 && matrix[ur][i] == 1) {
+		if (findPath(matrix, ur, i, visited)) {
+			return true;
+		} // if up element on above row has 1
+	}
+	return false;
+}
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+const matrix = [
+	[1, 0, 0, 0],
+	[1, 1, 0, 0],
+	[1, 1, 0, 0],
+	[0, 1, 1, 1],
+];
+const result = findPath(matrix, 0, 0);
+console.log(count, result);
