9ec179d9b51f2e8279035b1a3299b55a6a5dc4cb
aim3581 <51161705+aim3581@users.noreply.github.com>
2024-04-26 05:07:44 +0000

    find substring leetcode

---
+++ cmd/findSubstring.go
@@ -0,0 +1,46 @@
+package main
+import(
+	"fmt"
+	"slices"
+)
+
+func findSubstring(s string, words []string) []int {
+	if len(words) == 0 || len(s) ==0 {
+        return []int{}
+    }
+	wsize := len(words[0])
+	q := []int{}
+	result := []int{}
+	w := slices.Clone(words)
+	for i := 0; i < len(s) && i+wsize <= len(s); {
+		substr := s[i : i+wsize]
+		if slices.Contains(words, substr) {
+			ind := slices.Index(words, substr)
+			if slices.Contains(w, substr) {
+				j := slices.Index(w, substr)
+				w = append(w[:j], w[j+1:]...)
+				q = append(q, ind)
+			} else if len(q) > 0 {
+				w = append(w, words[q[0]])
+				q = q[1:]
+			}
+
+		} else {
+			q = []int{}
+			w = slices.Clone(words)
+			// fmt.Println(i, i+wsize, string(substr), q, w, words)
+			i = i + 1
+			continue
+		}
+		if len(q) == len(words) {
+			ind := i - ((len(words) - 1) * wsize)
+			w = append(w, words[q[0]])
+			q = q[1:]
+			w = slices.Clone(words)
+			result = append(result, ind)
+		}
+		fmt.Println(i, i+wsize, string(substr), q, w, words)
+		i = i + wsize
+	}
+	return result
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-
-	mstring "github.com/aim3581/dsa/mString"
+	"reflect"
 )
 
 func main() {
@@ -17,12 +16,45 @@ func main() {
 	// fmt.Println(t1)
 	// fmt.Println(t2)
 	// fmt.Println(tree.Same(t1, t2))
-	strs := []string{"bpfbhmipx", "bbbbbbbb", "abcabccb", "pwwkew"}
-	expecteds := []int{7, 1, 3, 3}
+	// strs := []string{"bpfbhmipx", "bbbbbbbb", "abcabccb", "pwwkew"}
+	// expecteds := []int{7, 1, 3, 3}
+	// for i := 0; i < len(strs); i++ {
+	// 	str := strs[i]
+	// 	n := mstring.UsingMap(str)
+	// 	fmt.Println(str, n, n == expecteds[i])
+	// }
+	strs := []string{
+		"barfoothefoobarman", 
+		"wordgoodgoodgoodbestword", 
+		"barfoofoobarthefoobarman",
+		"lingmindraboofooowingdingbarrwingmonkeypoundcake", 
+		"wordgoodgoodgoodbestword", 
+		"aaaaaaaaaaaaaa",
+	}
+	arr := [][]string{
+		[]string{"foo","bar"}, 
+		[]string{"word","good","best","word"}, 
+		[]string{"bar","foo","the"}, 
+		[]string{"fooo","barr","wing","ding","wing"}, 
+		[]string{"word","good","best","good"},
+		[]string{"aa", "aa"},
+	}
+	expecteds := [][]int{
+		[]int{0,9}, 
+		[]int{}, 
+		[]int{6,9,12}, 
+		[]int{13}, 
+		[]int{8},
+		[]int{0,1,2,3,4,5,6,7,8,9,10},
+	}
 	for i := 0; i < len(strs); i++ {
 		str := strs[i]
-		n := mstring.UsingMap(str)
-		fmt.Println(str, n, n == expecteds[i])
+		res:= findSubstring(str,arr[i])
+		if reflect.DeepEqual(res,expecteds[i]) {
+			fmt.Println("Passed")
+		}else{			
+		fmt.Println("Failed. expected: ", expecteds[i], " Got: ",res)
+		}
 	}
 
 }
